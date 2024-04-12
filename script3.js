6a1a6a2003c10f75dc535581ee5bb52f939a42d0
Kidus Mekuria <74838848+kidusm001@users.noreply.github.com>
2024-04-12 14:57:32 +0300

    works

--- script3.js
+++ script3.js
@@ -1,31 +1,58 @@
 const apiUrl = 'https://my-json-server.typicode.com/kidusm001/fetch-api-learn';
 const list = document.querySelector('ul.list');
+const addForm = document.querySelector('#add-todo');
 
 const getTodo = () => {
   fetch(`${apiUrl}/todos`)
     .then((resp) => resp.json())
     .then((todos) => {
       todos.forEach((todo) => {
-        const item = todo.body;
-        const li = document.createElement('li');
-        const span = document.createElement('span');
-        li.classList = 'item';
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        if (todo.completed) {
-          checkbox.checked = true;
-        }
-        span.appendChild(document.createTextNode(item));
-        li.appendChild(span);
-        li.appendChild(checkbox);
-        list.appendChild(li);
+        addTodoToDOM(todo);
       });
     })
     .catch((error) => console.log(`error: `, error));
 };
 
-const addTodo = (todo) => {
-  fetch(`${apiUrl}/todos`);
+function addTodoToDOM(todo) {
+  const item = todo.body;
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  li.classList = 'item';
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  if (todo.completed) {
+    checkbox.checked = true;
+  }
+  span.appendChild(document.createTextNode(item));
+  li.appendChild(span);
+  li.appendChild(checkbox);
+  li.setAttribute('data-id', todo.id);
+  list.appendChild(li);
+}
+
+const addTodo = (e) => {
+  e.preventDefault();
+  const todo = {
+    body: e.target.firstElementChild.value,
+    completed: false,
+  };
+  fetch(`${apiUrl}/todos`, {
+    method: 'POST',
+    body: JSON.stringify(todo),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => addTodoToDOM(data));
+};
+let todo = {
+  body: 'Test 1',
+  completed: true,
 };
 
-getTodo();
+function init() {
+  document.addEventListener('DOMContentLoaded', getTodo);
+  addForm.addEventListener('submit', addTodo);
+}
+init();
