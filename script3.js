c3711930f6baa7ef411cd4319c6fde8c197e519b
Kidus Mekuria <74838848+kidusm001@users.noreply.github.com>
2024-04-15 22:53:13 +0300

    noice things

--- script3.js
+++ script3.js
@@ -54,14 +54,61 @@ let todo = {
 
 const toggleCompleted = (e) => {
   if (e.target.classList.contains('item')) {
-    if (e.target.checked) {
+    const checkbox = e.target.querySelector('input[type="checkbox"]');
+    checkbox.checked = !checkbox.checked;
+    updateTodo(e.target.dataset.id, checkbox.checked);
+  }
+};
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/todos/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ completed }),
+  }).then((resp) => resp.json());
+};
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('item')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/todos/${id}`, {
+      method: 'DELETE',
+    })
+      .then((resp) => resp.json())
+      .then(() => e.target.remove());
+  }
+};
+
+const todoUpdate = async (e) => {
+  try {
+    if (e.target.classList.contains('item')) {
+      const checkbox = e.target.querySelector('input[type="checkbox"]');
+      checkbox.checked = !checkbox.checked;
+      const response = await fetch(`${apiUrl}/todos/${e.target.dataset.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          completed: checkbox.checked,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        throw new Error('Something went wrong');
+      }
     }
+  } catch (error) {
+    console.log('error: ', error);
   }
 };
 
 function init() {
   document.addEventListener('DOMContentLoaded', getTodo);
   addForm.addEventListener('submit', addTodo);
-  list.addEventListener('click', toggleCompleted);
+  list.addEventListener('click', todoUpdate);
+  list.addEventListener('dblclick', deleteTodo);
 }
 init();
